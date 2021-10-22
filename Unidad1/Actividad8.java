111dcb4e5271bce40b1f9aab6f7fda76436c332a
Lucas <80311627+Lucgome10@users.noreply.github.com>
2021-10-22 18:27:50 +0200

    Comentarios.

--- Unidad1/Actividad8.java
+++ Unidad1/Actividad8.java
@@ -1,11 +1,19 @@
 package ud1.activ8;
 
+/*
+Crea un programa que reciba desde los argumentos del método main() 1 número e imprima por
+consola el cubo de ese número. Utiliza el código 1 (System.exit(1)) cuando el programa finalice de
+forma correcta y el código -1 cuando la finalización no sea correcta.
+*/
+
 public class Actividad8 {
 
 	public static void main(String[] args) {
 		
+		
 		try {
-            int numero = Integer.parseInt(args[0]);			
+			//Es necesario convertir el argumento a int porque se recoge como String.
+            		int numero = Integer.parseInt(args[0]);			
 			System.out.println("El cubo es: " + (numero  *numero * numero));
 			System.exit(1);
 		} catch (Exception e) {
