0d04c6bb09f3b891da833b274190cf20834be157
Lucas <80311627+Lucgome10@users.noreply.github.com>
2021-10-22 18:25:21 +0200

    Comentarios añadidos.

--- Unidad1/Actividad7.java
+++ Unidad1/Actividad7.java
@@ -2,19 +2,27 @@
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+/*
+Elabora un programa en java que permita abrir cualquier aplicación y que pasados 5 segundos la
+cierre y el programa finalice.
+*/
+
 public class Act6 {
 
 	public static void main(String[] args) throws IOException {
 		
+		// Primero se comprueba si hay argumentos.
 		if (args.length==0) {
 			System.out.println("Amigo, no has introducido ningún argumento");
 		} else {
+			//Se inicia el proceso.
 			System.out.println("Se ha detectado el argumeto " + args[0]);
 			ProcessBuilder pb = new ProcessBuilder(args[0]);
 			System.out.println("Vamos a abrir el programa " + args[0]);
 			Process p = pb.start();
 
 			try {
+				//Se espera el plazo de 3 segundos y se destruye el proceso.
 				p.waitFor(3000, TimeUnit.MILLISECONDS);
 				p.destroy();
 				System.out.println("Destruyendo " + args[0] + " tras 3000ms");
